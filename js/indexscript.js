cc15f4d6c7687cd96520f42a97eddf793c30c269
GLKam1L <glkamil.17@gmail.com>
2023-02-11 21:56:09 +0300

    Initial commit

---
+++ js/indexscript.js
@@ -0,0 +1,12 @@
+function Input(){
+    login_ok = false;
+    var data1 = document.getElementsByName("inputdata1")[0].value;
+    data1 = data1.toUpperCase();
+    var data2 = document.getElementsByName("inputdata2")[0].value;
+    data2 = data2.toUpperCase();
+
+    if (data1 == "YULIANA" && data2 == "BEEPTOBEEP") {
+        login_ok=true;
+        window.location = "hello.html";}             // КУДА? Перебросит нас скрипт
+    if (login_ok == false) {
+        alert("Повнимательнее)");}}     // КОНЕЦ ПАРОЛЬНОГО ВХОДА
