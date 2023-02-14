c488e40b0b318e1717d0a45fb1fa276a02f266a7
GLKam1L <glkamil.17@gmail.com>
2023-02-14 14:53:33 +0300

    исправление

---
+++ js/prize2script.js
@@ -0,0 +1,12 @@
+function Input(){
+    login_ok = false;
+    var data1 = document.getElementsByName("inputdata1")[0].value;
+    data1 = data1.toUpperCase();
+    var data2 = document.getElementsByName("inputdata2")[0].value;
+    data2 = data2.toUpperCase();
+
+    if (data1 == "YULIANA" && data2 == "BEEPTOBEEP0907") {
+        login_ok=true;
+        window.location = "prize2.html";}             // КУДА? Перебросит нас скрипт
+    if (login_ok == false) {
+        alert("Повнимательнее)");}}     // КОНЕЦ ПАРОЛЬНОГО ВХОДА
