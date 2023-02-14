416d5527c01b43852ee5fc0d63cd3e4c40d43df7
GLKam1L <glkamil.17@gmail.com>
2023-02-14 14:57:54 +0300

    кнопка активна

--- js/prize2script.js
+++ js/prize2script.js
@@ -1,12 +1,5 @@
-function Input(){
-    login_ok = false;
-    var data1 = document.getElementsByName("inputdata1")[0].value;
-    data1 = data1.toUpperCase();
-    var data2 = document.getElementsByName("inputdata2")[0].value;
-    data2 = data2.toUpperCase();
+function Input() {
+    window.location = "Sercan.html";
+}
 
-    if (data1 == "YULIANA" && data2 == "BEEPTOBEEP0907") {
-        login_ok=true;
-        window.location = "Sercan.html";}             // КУДА? Перебросит нас скрипт
-    if (login_ok == false) {
-        alert("Повнимательнее)");}}     // КОНЕЦ ПАРОЛЬНОГО ВХОДА
+// КУДА? Перебросит нас скрипт
